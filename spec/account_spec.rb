4ba97d7bf9edd68d9c7d21db502acdc79e27095d
Mark McLeod <markmcleod45@gmail.com>
2022-01-04 12:16:03 +0000

    Add a deposit method that adds an amount to the balance of an account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,4 +8,10 @@ describe Account do
     expect(account.balance).to eq(0)
   end
 
+  describe '#deposit' do
+    it 'adds the deposit to the balance of the account' do
+      expect {account.deposit(1000) }.to change { account.balance }.by(1000)
+    end
+  end
+
 end
