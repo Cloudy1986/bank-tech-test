bab28054f9a749b71f404d61f09e2a1ec0b981e1
Mark McLeod <markmcleod45@gmail.com>
2022-01-05 13:20:51 +0000

    Add temp test for printing statement. It's failing on dates and need to refactor

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -2,13 +2,14 @@ require './lib/statement'
 
 describe Statement do
   # To do - need to work out how to mock account_transaction_history.
-end
 
-# describe '#print_statement' do
-#   it 'prints out the transaction history for the account' do
-#     account.deposit(1000)
-#     account.withdraw(500)
-#     account.withdraw(100)
-#     expect { account.print_statement }.to output.to_stdout
-#   end
-# end
+  #Temp test - failing on dates. Need to refactor
+  it 'prints out the transaction history for the account' do
+    account_transaction_history = [
+      {:date=>2022-01-05, :credit=>500, :debit=>nil, :balance=>500},
+      {:date=>2022-01-05, :credit=>nil, :debit=>100, :balance=>400}
+    ]
+    expect { Statement.print_statement(account_transaction_history) }.to output("date || credit || debit || balance\n" + "2022-01-05 || || 100 || 400\n" +
+      "2022-01-05 || 500 || || 500\n").to_stdout
+  end
+end
