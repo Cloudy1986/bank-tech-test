edeb28b750539b0f7ca1b30d8eebadb1cda86ad6
Mark McLeod <markmcleod45@gmail.com>
2022-01-05 13:59:00 +0000

    Fix broken test for printing out a statement

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,16 +1,12 @@
 require './lib/statement'
 
 describe Statement do
-  # To do - need to work out how to mock account_transaction_history.
-
-  #Temp test - failing on dates. Need to refactor
   it 'prints out the transaction history for the account' do
     account_transaction_history = [
-      {:date=>2022-01-05, :credit=>500, :debit=>nil, :balance=>500},
-      {:date=>2022-01-05, :credit=>nil, :debit=>100, :balance=>400}
+      {:date=>Date.today.strftime("%d/%m/%Y"), :credit=>500, :debit=>nil, :balance=>500},
+      {:date=>Date.today.strftime("%d/%m/%Y"), :credit=>nil, :debit=>100, :balance=>400}
     ]
-    p account_transaction_history
-    expect { Statement.print_statement(account_transaction_history) }.to output("date || credit || debit || balance\n" + "05/01/2022 || || 100.00 || 400.00\n" +
-      "05/01/2022 || 500.00 || || 500.00\n").to_stdout
+    expect { Statement.print_statement(account_transaction_history) }.to output("date || credit || debit || balance\n" + "05/01/2022 ||  || 100.00 || 400.00\n" +
+      "05/01/2022 || 500.00 ||  || 500.00\n").to_stdout
   end
 end
