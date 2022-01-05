3e7209746420912517d99eec41fc4264f8fdb3c5
Mark McLeod <markmcleod45@gmail.com>
2022-01-05 16:43:37 +0000

    Add feature test for Account and Statement classes working together

---
+++ spec/feature_test_spec.rb
@@ -0,0 +1,13 @@
+require './lib/account'
+
+describe 'Bank Account program' do
+  it 'creates an account, makes some deposits, makes some withdrawals and prints a statement' do
+    my_account = Account.new
+    my_account.deposit(1000)
+    my_account.deposit(2000)
+    my_account.withdraw(100)
+    my_account.withdraw(1500)
+    expect { Statement.print_statement(my_account.transaction_history) }.to output("date || credit || debit || balance\n" + "05/01/2022 ||  || 1500.00 || 1400.00\n" +
+      "05/01/2022 ||  || 100.00 || 2900.00\n" + "05/01/2022 || 2000.00 ||  || 3000.00\n" + "05/01/2022 || 1000.00 ||  || 1000.00\n").to_stdout
+  end
+end
