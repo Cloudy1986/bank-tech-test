aa15af2e097adb1b3c86c58ed335edecbbbc8534
Mark McLeod <markmcleod45@gmail.com>
2022-01-05 17:10:15 +0000

    Move displaying header into a method

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,7 +1,7 @@
 class Statement
 
   def self.print_statement(account_transaction_history)
-    puts 'date || credit || debit || balance'
+    header
     account_transaction_history.reverse.each do |transaction|
       puts "#{transaction[:date]} || #{format_number(transaction[:credit])} || #{format_number(transaction[:debit])} || #{format_number(transaction[:balance])}"
     end
@@ -11,6 +11,10 @@ class Statement
     private
     def format_number(number)
       '%.2f' % number if number.is_a?(Numeric)
+    end
+
+    def header
+      puts 'date || credit || debit || balance'
     end
   end
 end
